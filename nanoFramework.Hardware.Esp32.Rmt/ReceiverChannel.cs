a82ffc532665e6b05cfe9ebfd2487c4bd345910a
Adrian Soundy <adriansoundy@gmail.com>
2020-09-22 09:02:19 +1200

    Changes from review
    Fix case of parameters
    Fix Tab / spaces
    Removed channel property from TransmitterChannel as not required.
    Fix braces

--- nanoFramework.Hardware.Esp32.Rmt/ReceiverChannel.cs
+++ nanoFramework.Hardware.Esp32.Rmt/ReceiverChannel.cs
@@ -9,140 +9,143 @@ using System.Runtime.CompilerServices;
 
 namespace nanoFramework.Hardware.Esp32.Rmt
 {
-	/// <summary>
-	/// A class that can be used to Receive RMT items on ESP32
-	/// </summary>
-	public class ReceiverChannel : RmtChannel, IDisposable
-	{
-		#region Fields
+    /// <summary>
+    /// A class that can be used to Receive RMT items on ESP32
+    /// </summary>
+    public class ReceiverChannel : RmtChannel, IDisposable
+    {
+        #region Fields
 
-		[System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
-		private TimeSpan _receiveTimeout = new TimeSpan(0, 0, 1); // 1 sec
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        private TimeSpan _receiveTimeout = new TimeSpan(0, 0, 1); // 1 sec
 
-		#endregion Fields
+        #endregion Fields
 
-		/// <summary>
-		/// The receive time-out used when calling GetAllItems. Default 1 second.
-		/// </summary>
-		public TimeSpan ReceiveTimeout
-		{
-			get => _receiveTimeout;
-			set => _receiveTimeout = value;
-		}
-		/// <summary>
-		/// Public constructor to create receiver channel object.
-		/// </summary>
-		/// <param name="Gpio">The GPIO pin number that we want to use for receiving</param>
-		/// <param name="RmtBufferSize">The maximum number of RMT commands to be reserved in receiver input buffer. 
-		/// Default 100.</param>
-		public ReceiverChannel(int Gpio, int RmtBufferSize = 100)
-		{
-			// Default to 1MHZ tick ( 1us )
-			_clockDivider = 80;
+        /// <summary>
+        /// The receive time-out used when calling GetAllItems. Default 1 second.
+        /// </summary>
+        public TimeSpan ReceiveTimeout
+        {
+            get => _receiveTimeout;
+            set => _receiveTimeout = value;
+        }
+        /// <summary>
+        /// Public constructor to create receiver channel object.
+        /// </summary>
+        /// <param name="gpio">The GPIO pin number that we want to use for receiving</param>
+        /// <param name="rmtBufferSize">The maximum number of RMT commands to be reserved in receiver input buffer. 
+        /// Default 100.</param>
+        public ReceiverChannel(int gpio, int rmtBufferSize = 100)
+        {
+            // Default to 1MHZ tick ( 1us )
+            _clockDivider = 80;
 
-			// Initialise channel with GPIO and ring buffer size
-			_channel = NativeRxInit(Gpio, RmtBufferSize);
-		}
+            // Initialise channel with GPIO and ring buffer size
+            _channel = NativeRxInit(gpio, rmtBufferSize);
+        }
 
-		/// <summary>
-		/// Start receiving data on channel.
-		/// </summary>
-		public void Start(bool ClearBuffer)
-		{
-			NativeRxStart(ClearBuffer);
-		}
+        /// <summary>
+        /// Start receiving data on channel.
+        /// </summary>
+        /// <param name="clearBuffer">Clears buffer before starting.</param>
+        public void Start(bool clearBuffer)
+        {
+            NativeRxStart(clearBuffer);
+        }
 
-		/// <summary>
-		/// Stop receiving data on channel.
-		/// </summary>
-		public void Stop()
-		{
-			NativeRxStop();
-		}
+        /// <summary>
+        /// Stop receiving data on channel.
+        /// </summary>
+        public void Stop()
+        {
+            NativeRxStop();
+        }
 
-		/// <summary>
-		/// Enable / Disable filter for channel.
-		/// </summary>
-		/// <param name="Enable">True to Enable filter</param>
-		/// <param name="Threshold">Pulse width to ignore expressed in number of source clock cycles,
-		/// Value between 1-255</param>
+        /// <summary>
+        /// Enable / Disable filter for channel.
+        /// </summary>
+        /// <param name="enable">True to Enable filter</param>
+        /// <param name="threshold">Pulse width to ignore expressed in number of source clock cycles,
+        /// Value between 1-255</param>
 
-		public void EnableFilter(bool Enable, Byte Threshold)
-		{
-			NativeRxEnableFilter(Enable, Threshold);
-		}
+        public void EnableFilter(bool enable, Byte threshold)
+        {
+            NativeRxEnableFilter(enable, threshold);
+        }
 
-		/// <summary>
-		/// Set the Idle Threshold in ticks.
-		/// </summary>
-		/// <Remarks>
-		/// The receive process finishes(goes idle) when no edges have been detected for Idle Threshold clock cycles.</Remarks>
-		/// <param name="Threshold">Value between 1 and 65535 (0xFFFF)</param>
-		public void SetIdleThresold(UInt16 Threshold)
-		{
-			if (Threshold == 0)
-				throw new IndexOutOfRangeException();
+        /// <summary>
+        /// Set the Idle Threshold in ticks.
+        /// </summary>
+        /// <Remarks>
+        /// The receive process finishes(goes idle) when no edges have been detected for Idle Threshold clock cycles.</Remarks>
+        /// <param name="threshold">Value between 1 and 65535 (0xFFFF)</param>
+        public void SetIdleThresold(UInt16 threshold)
+        {
+            if (threshold == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
 
-			NativeRxSetIdleThresold(Threshold);
-		}
+            NativeRxSetIdleThresold(threshold);
+        }
 
-		/// <summary>
-		/// Get all RmtCommand items available.
-		/// </summary>
-		/// <remarks>If no signal received in time-out period then empty array will be returned.</remarks>
-		/// <returns>Return array of RMTCommand. <br/>
-		/// If no signal received in time-out period then empty array will be returned.
-		/// </returns>
-		public RmtCommand[] GetAllItems()
-		{
-			return NativeRxGetAllItems();
-		}
-		#region Destructor
+        /// <summary>
+        /// Get all RmtCommand items available.
+        /// </summary>
+        /// <remarks>If no signal received in time-out period then empty array will be returned.</remarks>
+        /// <returns>Return array of RMTCommand. <br/>
+        /// If no signal received in time-out period then empty array will be returned.
+        /// </returns>
+        public RmtCommand[] GetAllItems()
+        {
+            return NativeRxGetAllItems();
+        }
+        #region Destructor
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-		~ReceiverChannel() => Dispose(false);
+        ~ReceiverChannel() => Dispose(false);
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
 #pragma warning disable S4200 // Native methods should be wrapped
-		protected virtual void Dispose(bool disposing) => NativeRxDispose();
+        protected virtual void Dispose(bool disposing) => NativeRxDispose();
 #pragma warning restore S4200 // Native methods should be wrapped
 
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 
-		#endregion Destructor
+        #endregion Destructor
 
-		#region Native calls
+        #region Native calls
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern int NativeRxInit(int gpio, int rmtBufferSize);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern int NativeRxInit(int gpio, int rmtBufferSize);
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern void NativeRxStart(bool ClearBuffer);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeRxStart(bool clearBuffer);
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern void NativeRxStop();
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeRxStop();
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern int NativeRxGetRingBufferCount();
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern int NativeRxGetRingBufferCount();
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern RmtCommand[] NativeRxGetAllItems();
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern RmtCommand[] NativeRxGetAllItems();
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern void NativeRxEnableFilter(bool Enable, Byte Threshold);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeRxEnableFilter(bool enable, Byte threshold);
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern void NativeRxSetIdleThresold(UInt16 Threshold);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeRxSetIdleThresold(UInt16 threshold);
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern void NativeRxDispose();
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeRxDispose();
 
-		#endregion  Native calls
-	}
+        #endregion  Native calls
+    }
 
 }
--- nanoFramework.Hardware.Esp32.Rmt/RmtChannel.cs
+++ nanoFramework.Hardware.Esp32.Rmt/RmtChannel.cs
@@ -9,80 +9,80 @@ using System.Runtime.CompilerServices;
 
 namespace nanoFramework.Hardware.Esp32.Rmt
 {
-	/// <summary>
-	/// Base class for a RMT channel
-	/// </summary>
-	public class RmtChannel
-	{
-		#region Fields
+    /// <summary>
+    /// Base class for a RMT channel
+    /// </summary>
+    public class RmtChannel
+    {
+        #region Fields
 
-		/// <summary>
-		/// Assigned RMT channel, assigned when channel created.
-		/// </summary>
-		[System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
-		protected int _channel;
+        /// <summary>
+        /// Assigned RMT channel, assigned when channel created.
+        /// </summary>
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        protected int _channel;
 
-		/// <summary>
-		/// Clock divider used on channel
-		/// </summary>
-		[System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
-		protected byte _clockDivider = 80;
+        /// <summary>
+        /// Clock divider used on channel
+        /// </summary>
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        protected byte _clockDivider = 80;
 
-		/// <summary>
-		/// RMT channel source clock
-		/// </summary>
-		[System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
-		protected SourceClock _sourceClock = SourceClock.APB;
+        /// <summary>
+        /// RMT channel source clock
+        /// </summary>
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        protected SourceClock _sourceClock = SourceClock.APB;
 
-		/// <summary>
-		/// The number of RMT channels available.
-		/// </summary>
-		protected const byte _NumberRmtChannels = 8;
+        /// <summary>
+        /// The number of RMT channels available.
+        /// </summary>
+        protected const byte _NumberRmtChannels = 8;
 
-		/// <summary>
-		/// The largest channel number. Channel numbers start at 0.
-		/// </summary>
-		protected const byte _MaxChannelNumber = _NumberRmtChannels - 1;
+        /// <summary>
+        /// The largest channel number. Channel numbers start at 0.
+        /// </summary>
+        protected const byte _MaxChannelNumber = _NumberRmtChannels - 1;
 
-		#endregion Fields
+        #endregion Fields
 
-		/// <summary>
-		/// The channel number we are using
-		/// </summary>
-		public int Channel
-		{
-			get => _channel;
-			private set => _channel = value;
-		}
+        /// <summary>
+        /// The channel number we are using
+        /// </summary>
+        public int Channel
+        {
+            get => _channel;
+            private set => _channel = value;
+        }
 
-		/// <summary>
-		/// The value can be between 1 and 255.
-		/// </summary>
-		public byte ClockDivider
-		{
-			get => _clockDivider;
-			set
-			{
-				_clockDivider = value;
-				NativeSetClockDivider(_clockDivider);
-			}
-		}
+        /// <summary>
+        /// The value can be between 1 and 255.
+        /// </summary>
+        public byte ClockDivider
+        {
+            get => _clockDivider;
+            set
+            {
+                _clockDivider = value;
+                NativeSetClockDivider(_clockDivider);
+            }
+        }
 
-		/// <summary>
-		/// The source clock. Only the 80MHz APB clock is currently supported.
-		/// </summary>
-		public SourceClock SourceClock
-		{
-			get => _sourceClock;
-			set => _sourceClock = value;
-		}
+        /// <summary>
+        /// The source clock. Only the 80MHz APB clock is currently supported.
+        /// </summary>
+        public SourceClock SourceClock
+        {
+            get => _sourceClock;
+            set => _sourceClock = value;
+        }
 
-		#region native calls
+        #region native calls
 
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		private extern void NativeSetClockDivider(byte value);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeSetClockDivider(byte value);
 
-		#endregion native calls
+        #endregion native calls
 
-	}
+    }
 }
