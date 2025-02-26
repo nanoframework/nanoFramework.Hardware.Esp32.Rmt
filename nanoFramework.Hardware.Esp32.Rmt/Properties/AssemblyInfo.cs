cf69f61601d29180c525067d88701b44363f46e1
Cory Charlton <CoryCharlton@users.noreply.github.com>
2025-02-26 09:21:17 -0800

    Moving RmtCommand serialization to RmtCommandSerializer

--- nanoFramework.Hardware.Esp32.Rmt/Properties/AssemblyInfo.cs
+++ nanoFramework.Hardware.Esp32.Rmt/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -18,3 +19,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+[assembly: InternalsVisibleTo("nanoFramework.Hardware.Esp32.Rmt.Benchmarks, PublicKey=00240000048000009400000006020000002400005253413100040000010001001120aa3e809b3da4f65e1b1f65c0a3a1bf6335c39860ca41acb3c48de278c6b63c5df38239ec1f2e32d58cb897c8c174a5f8e78a9c0b6087d3aef373d7d0f3d9be67700fc2a5a38de1fb71b5b6f6046d841ff35abee2e0b0840a6291a312be184eb311baff5fef0ff6895b9a5f2253aed32fb06b819134f6bb9d531488a87ea2")]
+[assembly: InternalsVisibleTo("NFUnitTest, PublicKey=00240000048000009400000006020000002400005253413100040000010001001120aa3e809b3da4f65e1b1f65c0a3a1bf6335c39860ca41acb3c48de278c6b63c5df38239ec1f2e32d58cb897c8c174a5f8e78a9c0b6087d3aef373d7d0f3d9be67700fc2a5a38de1fb71b5b6f6046d841ff35abee2e0b0840a6291a312be184eb311baff5fef0ff6895b9a5f2253aed32fb06b819134f6bb9d531488a87ea2")]
---
+++ nanoFramework.Hardware.Esp32.Rmt/RmtCommandSerializer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+#nullable enable
+namespace nanoFramework.Hardware.Esp32.Rmt
+{
+    [ExcludeType]
+    internal static class RmtCommandSerializer
+    {
+        /// <summary>
+        /// Serialize commands to rmt_item32_t native byte format
+        /// </summary>
+        /// <returns>The serialized commands</returns>
+        public static byte[] SerializeCommands(ArrayList commands)
+        {
+            int i = 0;
+            int remaining;
+            byte[] binaryCommands = new byte[commands.Count * 4];
+            foreach (var cmd in commands)
+            {
+                // First pair
+                if ((cmd as RmtCommand).Duration0 <= 255)
+                {
+                    binaryCommands[0 + i] = (byte)(cmd as RmtCommand).Duration0;
+                    binaryCommands[1 + i] = (byte)((cmd as RmtCommand).Level0 == true ? 128 : 0);
+                }
+                else
+                {
+                    remaining = (cmd as RmtCommand).Duration0 % 256;
+                    binaryCommands[0 + i] = (byte)(remaining);
+                    binaryCommands[1 + i] = (byte)(((cmd as RmtCommand).Level0 ? 128 : 0) + (((cmd as RmtCommand).Duration0 - remaining) / 256));
+                }
+
+                // Second pair
+                if ((cmd as RmtCommand).Duration1 <= 255)
+                {
+                    binaryCommands[2 + i] = (byte)(cmd as RmtCommand).Duration1;
+                    binaryCommands[3 + i] = (byte)((cmd as RmtCommand).Level1 ? 128 : 0);
+                }
+                else
+                {
+                    remaining = (cmd as RmtCommand).Duration1 % 256;
+                    binaryCommands[2 + i] = (byte)(remaining);
+                    binaryCommands[3 + i] = (byte)(((cmd as RmtCommand).Level1 ? 128 : 0) + (((cmd as RmtCommand).Duration1 - remaining) / 256));
+                }
+                i += 4;
+            }
+            return binaryCommands;
+        }
+    }
+}
